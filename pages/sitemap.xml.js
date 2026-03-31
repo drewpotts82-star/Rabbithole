9e1b20fc096aface8fd1be0586aea49bf8b83653
Drew <your@email.com>
2026-04-01 09:07:19 +1000

    update sitemap with travel destinations

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,5 @@
 import topics from '../data/topics';
+import destinations from '../data/destinations';
 
 const BASE_URL = 'https://www.rabbitholevideo.com';
 
@@ -13,6 +14,12 @@ export async function getServerSideProps({ res }) {
     <priority>${t.tier === 'gold' ? '0.9' : '0.8'}</priority>
   </url>`).join('\n');
 
+  const destPages = destinations.map((d) => `  <url>
+    <loc>${BASE_URL}/destination/${d.slug}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>${d.tier === 'gold' ? '0.9' : '0.8'}</priority>
+  </url>`).join('\n');
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
@@ -21,11 +28,17 @@ export async function getServerSideProps({ res }) {
     <priority>1.0</priority>
   </url>
   <url>
+    <loc>${BASE_URL}/travel</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
     <loc>${BASE_URL}/pro</loc>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
 ${topicPages}
+${destPages}
 </urlset>`;
 
   res.setHeader('Content-Type', 'text/xml');
