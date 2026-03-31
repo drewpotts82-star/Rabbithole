37c7aa049f1ffb25ed0df3e8df01e16ee2b2d949
Drew <your@email.com>
2026-03-31 14:42:18 +1000

    add google verification

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#111110" />
+        <meta name="google-site-verification" content="qrhhnGiuiWrXrTByX288ZcW6Gf87-6bIA7mLfj87J50" />
+        <meta property="og:site_name" content="RabbitHole" />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
